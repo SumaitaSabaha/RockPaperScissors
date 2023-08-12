93d5bd5f75123cc81eba5fe0f0501f9790846e1b
SumaitaSabaha <sumaitasabaha9@gmail.com>
2023-08-12 15:45:43 -0500

    Added playRound and game function

--- game.js
+++ game.js
@@ -5,8 +5,47 @@ function getComputerChoice(arr) {
     const item = arr[index];
     return item;
 }
-
 const choices = ["Rock", "Paper", "Scissors"];
-console.log("Testing");
 const result = getComputerChoice(choices);
-console.log("Computer's choice is", result);
+
+let pscore = 0;
+let cscore = 0;
+
+function playRound(playerSelection, computerSelection) {
+
+    if((computerSelection === "Paper" && playerSelection == "Scissors") ||
+    (computerSelection === "Scissors" && playerSelection == "Rock") ||
+    (computerSelection === "Rock" && playerSelection == "Paper")) 
+    {
+        pscore++;
+
+    }
+    else if ((playerSelection === "Paper" && computerSelection == "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection == "Rock") ||
+    (playerSelection === "Rock" && computerSelection == "Paper")) {
+        cscore++;
+    }
+}
+
+function game(number) {
+    for(i = 0; i < number ; i++) {
+        let input = prompt("Enter Rock, Paper, or scissors");
+        console.log("Computer's choice is", result);
+        playRound(input.charAt(0).toUpperCase() + input.slice(1).toLowerCase(), result); 
+        console.log("Your score is ",pscore, "computers score is ", cscore);
+    }
+
+    if(pscore > cscore) {
+        console.log("You win!");
+    }
+    else if (pscore == cscore) {
+        console.log("It's a tie!");
+    }
+    else {
+        console.log("You lose!");
+    }
+}
+let games = prompt("How many times do you wanna play? ");
+game(Number(games));
+
+
