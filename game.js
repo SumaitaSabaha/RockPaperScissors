08e08096043707210b71f738320b5f25c511150a
SumaitaSabaha <sumaitasabaha9@gmail.com>
2023-08-23 15:42:03 -0500

    added all the game functionalites

--- game.js
+++ game.js
@@ -1,41 +1,82 @@
-// Generating either "Rock", "Paper", or "Scissors"
+// // Generating either "Rock", "Paper", or "Scissors"
 
-function getComputerChoice(arr) {
-    const index = Math.floor(Math.random()*arr.length);
-    const item = arr[index];
-    return item;
+const playerFist = document.querySelector('.player');
+const computerFist = document.querySelector('.computer');
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const pscore = document.querySelector('.player-score');
+const cscore = document.querySelector('.computer-score');
+const final = document.querySelector('.winner');
+
+let playerScore = 0;
+let computerScore = 0;
+
+rockBtn.addEventListener('click', () => playRound('rock'));
+paperBtn.addEventListener('click', () => playRound('paper'));
+scissorsBtn.addEventListener('click', () => playRound('scissors'));
+
+function playRound(playerChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+
+    const computerGif = {
+        rock: 'cr.gif',
+        paper: 'cp.gif',
+        scissors: 'cs.gif' 
+    };
+
+    playerFist.src = `${playerChoice}.gif`;
+    computerFist.src =  computerGif[computerChoice];
+
+    const result = determineWinner(playerChoice, computerChoice);
+
+    if(result === "You win!") {
+        playerScore++;
+    }
+    else if(result === "Computer wins") {
+        computerScore++;
+    }
+
+    pscore.textContent = `Player: ${playerScore}`;
+    cscore.textContent = `Computer: ${computerScore}`;
+
+    if(playerScore === 5 || computerScore === 5) {
+        score(playerScore, computerScore);
+    }
+
 }
-const choices = ["Rock", "Paper", "Scissors"];
 
-let pscore = 0;
-let cscore = 0;
-
-function playRound(playerSelection, computerSelection) {
-
-    if((computerSelection === "Paper" && playerSelection == "Scissors") ||
-    (computerSelection === "Scissors" && playerSelection == "Rock") ||
-    (computerSelection === "Rock" && playerSelection == "Paper")) 
+function determineWinner(playerChoice, computerChoice) {
+    if(
+    (computerChoice === "paper" && playerChoice == "scissors") ||
+    (computerChoice === "scissors" && playerChoice == "rock") ||
+    (computerChoice === "rock" && playerChoice == "paper")
+    ) 
     {
-        pscore++;
+        return "You win!";
 
     }
-    else if ((playerSelection === "Paper" && computerSelection == "Scissors") ||
-    (playerSelection === "Scissors" && computerSelection == "Rock") ||
-    (playerSelection === "Rock" && computerSelection == "Paper")) 
+    else if (
+    (playerChoice === "paper" && computerChoice == "scissors") ||
+    (playerChoice === "scissors" && computerChoice == "rock") ||
+    (playerChoice === "rock" && computerChoice == "paper")
+    ) 
     {
-        cscore++;
+        return "Computer wins";
     }
 }
-let result = getComputerChoice(choices);
-playRound(input.charAt(0).toUpperCase() + input.slice(1).toLowerCase(), result); 
-if(pscore > cscore) {
-    console.log("You win!");
-}
-else if (pscore == cscore) {
-    console.log("It's a tie!");
-}
-else {
-    console.log("You lose!");
+function score(playerScore, computerScore) {
+    console.log("Updating final result...");
+    if(playerScore === 5) {
+        final.textContent = "Result: You win!" 
+    }
+    else if(playerScore === computerScore) {
+        final.textContent= "Result: It's a tie!"
+    }
+    else if(computerScore === 5) {
+        final.textContent= "Result: You lose!"
+    }
 }
 
 
